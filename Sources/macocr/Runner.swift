c5006a0bdc1d4dde51d036a585b5727d601d2093
Karl Engelhardt <karl.engelhardt@okfn.de>
2023-01-17 20:04:53 +0100

    ♻️ More CLI-Opts, Process only pdf, write output per page into dir

--- Sources/macocr/Runner.swift
+++ Sources/macocr/Runner.swift
@@ -12,98 +12,79 @@ import Foundation
 
 @available(macOS 11.0, *)
 class Runner {
-
-
-    struct ImageFileType {
-        var uti: CFString
-        var fileExtention: String
-
-        // This list can include anything returned by CGImageDestinationCopyTypeIdentifiers()
-        // I'm including only the popular formats here
-        static let bmp = ImageFileType(uti: kUTTypeBMP, fileExtention: "bmp")
-        static let gif = ImageFileType(uti: kUTTypeGIF, fileExtention: "gif")
-        static let jpg = ImageFileType(uti: kUTTypeJPEG, fileExtention: "jpg")
-        static let png = ImageFileType(uti: kUTTypePNG, fileExtention: "png")
-        static let tiff = ImageFileType(uti: kUTTypeTIFF, fileExtention: "tiff")
-    }
-
-    func convertPDF(at sourceURL: URL, to destinationURL: URL, fileType: ImageFileType, dpi: CGFloat = 200) throws -> [URL] {
+    
+    
+    
+    static func convertPDF(at sourceURL: URL, to destinationURL: URL, languages: [String], dpi: CGFloat = 200, customWords: [String]?) throws -> [[String]] {
+        
         let pdfDocument = CGPDFDocument(sourceURL as CFURL)!
         let colorSpace = CGColorSpaceCreateDeviceRGB()
         let bitmapInfo = CGImageAlphaInfo.noneSkipLast.rawValue
-
-        var urls = [URL](repeating: URL(fileURLWithPath : "/"), count: pdfDocument.numberOfPages)
+        
+        var texts = [[String]](repeating: [], count: pdfDocument.numberOfPages)
         DispatchQueue.concurrentPerform(iterations: pdfDocument.numberOfPages) { i in
             // Page number starts at 1, not 0
             let pdfPage = pdfDocument.page(at: i + 1)!
-
+            
             let mediaBoxRect = pdfPage.getBoxRect(.mediaBox)
             let scale = dpi / 72.0
             let width = Int(mediaBoxRect.width * scale)
             let height = Int(mediaBoxRect.height * scale)
-
+            
             let context = CGContext(data: nil, width: width, height: height, bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo)!
             context.interpolationQuality = .high
             context.setFillColor(.white)
             context.fill(CGRect(x: 0, y: 0, width: width, height: height))
             context.scaleBy(x: scale, y: scale)
             context.drawPDFPage(pdfPage)
-
+            
             let image = context.makeImage()!
-            let imageName = sourceURL.deletingPathExtension().lastPathComponent
-            let imageURL = destinationURL.appendingPathComponent("\(imageName)-Page\(i+1).\(fileType.fileExtention)")
-
-            let imageDestination = CGImageDestinationCreateWithURL(imageURL as CFURL, fileType.uti, 1, nil)!
-            CGImageDestinationAddImage(imageDestination, image, nil)
-            CGImageDestinationFinalize(imageDestination)
-
-            urls[i] = imageURL
+            texts[i] = processImage(imgRef: image, languages: languages, customWords: customWords)
+            let pageOutputFile = destinationURL.appendingPathComponent("Page\(i+1).txt")
+            do {
+                try texts[i].joined(separator: "\n").write(to: pageOutputFile, atomically: true, encoding: String.Encoding.utf8)
+            } catch {
+                print("Failed to write txt-file for Page \(i+1): \(error).")
+            }
+            print("Finished Page \(i+1)")
         }
-        return urls
+        return texts
     }
-
-
-static func run(files: [String]) -> Int32 {
-
-
-    // Flag ideas:
-    // --version
-    // Print REVISION
-    // --langs
-    //guard let langs = VNRecognizeTextRequest.supportedRecognitionLanguages(for: .accurate, revision: REVISION)
-    // --fast (default accurate)
-    // --fix (default no language correction)
-    let urls = files.map {
-        URL(fileURLWithPath: $0)
-    }
-
-    for url in urls {
-
-        let img = NSImage(byReferencing: url)
-
-
-
-    let desc = img.debugDescription
-        guard let imgRef = img.cgImage(forProposedRect: nil, context: nil, hints: nil) else {
-            fputs("Error: failed to convert NSImage to CGImage for '\(url)'\n", stderr)
-            return 1
-        }
-
+    
+    static func processImage(imgRef: CGImage, languages: [String], customWords: [String]?) -> [String] {
+        var text: [String] = [];
         let request = VNRecognizeTextRequest { (request, error) in
             let observations = request.results as? [VNRecognizedTextObservation] ?? []
-            let obs : [String] = observations.map { $0.topCandidates(1).first?.string ?? ""}
-            try? obs.joined(separator: "\n").write(to: url.appendingPathExtension("md"), atomically: true, encoding: String.Encoding.utf8)
-
-            fputs("got page obs is \(obs)", stderr)
+            text = observations.map { $0.topCandidates(1).first?.string ?? ""}
         }
         request.recognitionLevel = VNRequestTextRecognitionLevel.accurate // or .fast
-        request.usesLanguageCorrection = true
         request.revision = VNRecognizeTextRequestRevision2
-        request.recognitionLanguages = ["de"]
-        request.customWords = ["der", "Der", "Name"]
-
+        request.usesLanguageCorrection = true
+        request.recognitionLanguages = languages
+        if let customWords = customWords {
+            request.customWords = customWords
+        }
+        
         try? VNImageRequestHandler(cgImage: imgRef, options: [:]).perform([request])
+        return text
     }
-    return 0
-}
+    
+    static func run(file: String, out_path: String, wordfile: String?, languages: [String]) -> Int32 {
+        var customWords: [String]? = nil;
+        if let wordfile = wordfile {
+            do {
+                let customWordList = try String(contentsOfFile: wordfile, encoding: .utf8)
+                
+                customWords = customWordList
+                    .trimmingCharacters(in: .whitespacesAndNewlines)
+                    .components(separatedBy: .newlines)
+            } catch {
+                print("Failed to read wordlist: \(error)")
+            }
+        }
+        
+        let _ = (try? convertPDF(at: URL.init(fileURLWithPath: file), to: URL.init(fileURLWithPath: out_path), languages: languages, customWords: customWords))!
+        
+        return 0
+    }
 }
--- Sources/macocr/start.swift
+++ Sources/macocr/start.swift
@@ -1,20 +1,26 @@
 import ArgumentParser
 @available(macOS 11.0, *)
 @main
-struct Repeat: ArgumentParser.ParsableCommand {
+struct MacOCR: ArgumentParser.ParsableCommand {
     @Flag(inversion: FlagInversion.prefixedNo, help: "fast")
     var fast = false
-
-
-    // @Option(name: .shortAndLong, help: "User word file")
-    // var wordfile: String?
-
-    @Argument(completion: .file(extensions: ["jpg","jpeg","png","tiff"]))
-    var files: [String] = []
-
-
+    
+    
+    @Option(name: .shortAndLong, help: "User word file")
+    var wordfile: String?
+    
+    @Option(name: .shortAndLong, help: "Language Code of the languages for OCR. Can be speficied multiple times, not setting a language disables the NLP-Based LanguageCorrection")
+    var language: [String] = []
+    
+    @Argument(completion: .file(extensions: ["pdf"]))
+    var file: String
+    
+    @Argument()
+    var out_path: String
+    
     mutating func run() throws {
-        Runner.run( files: files)
-
+        print("\(language)")
+        Runner.run( file: file, out_path: out_path, wordfile: wordfile, languages: language)
+        
     }
 }
